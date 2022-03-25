9b7f6810752822f8d7f7025d6bd3bc3c548a1261
Yaniv Blum <jungob94@gmail.com>
2022-03-25 11:59:18 +0300

    receiver compiles no hammid decoding yet

--- Receiver/ReceiverHelper.h
+++ Receiver/ReceiverHelper.h
@@ -3,7 +3,26 @@
 
 #include "../Common/Utils.h"
 
+typedef struct {
+	SOCKET socket;
+	int port;
+	char* ip;
+	FILE* file;
+	int encoded_message_size;
+	int decoded_message_size;
+	uint32_t messageHamming;
+	char filename[MAX_FILE_NAME_LEN];
+	bool quit;
+	char* encoded_message;
+	char* origin_message;
+	char* fixed_message;
+	int bits_fixed;
+}Receiver_Params;
 
-void PrintOutput(int blocks, int errors);
+void InitServerParams(char* argv[], Receiver_Params* receiver_p);
+void OpenInputFile(Receiver_Params* receiver_p);
+void PrintOutput(Receiver_Params* receiver_p);
+void AllocateBuffersSizes(Receiver_Params* receiver_p, char msg_str_size[10]);
+void CleanReceiverRound(Receiver_Params* receiver_p);
 
 #endif
